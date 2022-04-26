d3dcc616943eda4e7bd33fcc6727d12b58be890a
MyascoFP <ivansavchuk20011@gmail.com>
2022-04-26 16:41:15 +0300

    Meta change

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,15 @@
-from django.forms import ValidationError
-from django.utils import timezone
-
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
 from users.models import User
+
+
+def my_year_validator(value):
+        if value < 0 or value > timezone.now().year:
+            raise ValidationError(('%(value)s is not a correcrt year!'),
+                params={'value': value},
+            )
 
 
 class Genre(models.Model):
@@ -23,13 +29,6 @@ class Category(models.Model):
 
 
 class Title(models.Model):
-    def my_year_validator(value):
-        if value < 0 or value > timezone.now().year:
-            raise ValidationError(('%(value)s is not a correcrt year!'),
-                params={'value': value},
-            )
-
-    
     name = models.CharField(
         'Название',
         max_length=300,
@@ -87,9 +86,6 @@ class Review(models.Model):
         auto_now_add=True,
     )
 
-    def __str__(self) -> str:
-        return self.text
-
     class Meta:
         ordering = ['-pub_date']
         constraints = [
@@ -97,6 +93,9 @@ class Review(models.Model):
                 fields=['author', 'title'],
                 name="unique_review")
         ]
+
+    def __str__(self) -> str:
+        return self.text
 
 
 class Comment(models.Model):
